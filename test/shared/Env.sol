6a631bcc7bae13b7d2e4e276bc57c8e5555a0ec3
ltran <loris.tran@epfl.ch>
2023-10-15 19:06:07 +0200

    chnaged some rpc urls for test

--- test/shared/Env.sol
+++ test/shared/Env.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-uint256 constant forkBlockMainnet = 16200000;
-string constant forkRpcUrlMainnet = "https://rpc.ankr.com/eth";
-uint256 constant forkBlockPolygon = 37610791;
-string constant forkRpcUrlPolygon = "https://rpc.ankr.com/polygon";
+uint256 constant forkBlockMainnet = 18357100;
+string constant forkRpcUrlMainnet = "https://eth.llamarpc.com";
+uint256 constant forkBlockPolygon = 48753867;
+string constant forkRpcUrlPolygon = "https://polygon-rpc.com";
